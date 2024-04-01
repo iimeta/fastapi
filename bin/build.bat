488db4ffc13f0e6a6929d346fd6a46bce40d98df
Loka <loka@iim.ai>
2024-04-02 00:34:11 +0800

    更新脚本

--- bin/build.bat
+++ bin/build.bat
@@ -2,22 +2,9 @@
 cd ../
 go env -w GO111MODULE=on
 
-SET yyyy=%date:~,4%
-SET MM=%date:~5,2%
-SET dd=%date:~8,2%
-SET yyyyMMdd=%yyyy%%MM%%dd%
-SET yyyyMMdd=%yyyyMMdd: =%
-
-SET HH=%time:~0,2%
-SET mm=%time:~3,2%
-SET ss=%time:~6,2%
-SET HHmmss=%HH%%mm%%ss%
-
-SET timestamp=%yyyyMMdd%%HHmmss%
-
 SET GOOS=linux
 SET GOARCH=amd64
 
 echo build start
-go build -ldflags "-X main.Version=v0.1.0 -X main.Build=%timestamp%" -o ./bin/
+go build -ldflags "-X main.Version=v0.1.0 -X main.Build=2024" -o ./bin/
 echo build finish.
