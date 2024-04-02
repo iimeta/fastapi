1ff65d77685b0bb892a4387429dbf4c26d41d570
Loka <loka@iim.ai>
2024-04-02 13:42:20 +0800

    更新脚本

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,8 @@ cd ../
 export GOROOT=/usr/local/go
 export PATH=${PATH}:${GOROOT}/bin
 
+go env -w GOPROXY=https://goproxy.cn,direct
+
 timestamp=$(date '+%Y%m%d%H%M%S')
 
 go build -ldflags "-X main.Version=v0.1.0 -X main.Build=$timestamp" -o ./bin/
