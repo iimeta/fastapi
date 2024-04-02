d8ddb696c9d91a8410cb91eae2cbc4c2c6eb89bb
Loka <loka@iim.ai>
2024-04-02 11:24:19 +0800

    更新脚本

--- bin/build.bat
+++ bin/build.bat
@@ -1,10 +1,25 @@
 @echo off
 cd ../
+
 go env -w GO111MODULE=on
+go env -w GOPROXY=https://goproxy.cn,direct
+
+SET yyyy=%date:~,4%
+SET MM=%date:~5,2%
+SET dd=%date:~8,2%
+SET yyyyMMdd=%yyyy%%MM%%dd%
+SET yyyyMMdd=%yyyyMMdd: =%
+
+SET HH=%time:~0,2%
+SET mm=%time:~3,2%
+SET ss=%time:~6,2%
+SET HHmmss=%HH%%mm%%ss%
+
+SET timestamp=%yyyyMMdd%%HHmmss%
 
 SET GOOS=linux
 SET GOARCH=amd64
 
 echo build start
-go build -ldflags "-X main.Version=v0.1.0 -X main.Build=2024" -o ./bin/
+go build -ldflags "-X main.Version=v0.1.0 -X main.Build=%timestamp%" -o ./bin/
 echo build finish.
