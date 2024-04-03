cec05a5c3c8db30bc56051be965972b84fdd02cb
Loka <loka@iim.ai>
2024-04-03 09:50:56 +0800

    更新脚本

--- bin/build.bat
+++ bin/build.bat
@@ -4,22 +4,14 @@ cd ../
 go env -w GO111MODULE=on
 go env -w GOPROXY=https://goproxy.cn,direct
 
-SET yyyy=%date:~,4%
-SET MM=%date:~5,2%
-SET dd=%date:~8,2%
-SET yyyyMMdd=%yyyy%%MM%%dd%
-SET yyyyMMdd=%yyyyMMdd: =%
-
-SET HH=%time:~0,2%
-SET mm=%time:~3,2%
-SET ss=%time:~6,2%
-SET HHmmss=%HH%%mm%%ss%
-
-SET timestamp=%yyyyMMdd%%HHmmss%
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "timestamp=%YYYY%%MM%%DD%%HH%%Min%%Sec%"
 
 SET GOOS=linux
 SET GOARCH=amd64
 
 echo build start
-go build -ldflags "-X main.Version=v0.1.0 -X main.Build=%timestamp%" -o ./bin/
+go build -ldflags "-X main.Version=v0.1.1 -X main.Build=%timestamp%" -o ./bin/
 echo build finish.
